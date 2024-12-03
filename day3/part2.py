6c1dca8027e3d2a90019a9370dc69a385f896464
RealDragonMA <realdragonma@gmail.com>
2024-12-03 11:25:28 +0100

    day4

---
+++ day3/part2.py
@@ -0,0 +1,28 @@
+import re
+from operator import mul
+from functools import reduce
+
+def getLines(file):
+    with open(file) as file:
+        lines = [line.rstrip() for line in file]
+    return lines
+
+def solve():
+    lines = "".join(getLines("./input.txt"))
+    # Find all mul
+
+    x = re.findall("don't\\(\\)(.*?)do\\(\\)", lines)
+    for dontdo in x:
+        lines = lines.replace(dontdo, "")
+
+    ### Part 1 ###
+    x = re.findall("mul\\([0-9]{1,3},[0-9]{1,3}\\)", lines)
+    # Get only numbers e.g: [['2', '4'], ['5', '5'], ['11', '8'], ['8', '5']]
+    x = [re.findall("[0-9]{1,3}", digits) for digits in x]
+    # Convert to int : [[2, 4], [5, 5], [11, 8], [8, 5]]
+    x = [[int(digit) for digit in digits] for digits in x]
+    # Sum all multiplications
+    x = sum(reduce(mul, digits) for digits in x)
+    print(x)
+
+solve()
